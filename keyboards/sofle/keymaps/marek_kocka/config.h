1053a08ac204f63fbb4cb549caa1a317123f5b3e
Marek Cerny <me@marekcerny.com>
2021-11-06 15:01:04 +0100

    new layout

--- keyboards/sofle/keymaps/marek_kocka/config.h
+++ keyboards/sofle/keymaps/marek_kocka/config.h
@@ -4,3 +4,17 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+
+#define MOUSEKEY_INITIAL_SPEED     200
+#define MOUSEKEY_BASE_SPEED        2000
+#define MOUSEKEY_DECELERATED_SPEED 800
+#define MOUSEKEY_ACCELERATED_SPEED 5000
+#define MK_3_SPEED
+
+#define MK_C_OFFSET_0      4
+#define MK_C_INTERVAL_0        16
+#define MK_C_OFFSET_1      12
+#define MK_C_INTERVAL_1        16
+#define MK_C_OFFSET_2      40
+#define MK_C_INTERVAL_2        16
